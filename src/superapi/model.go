a8e8d2afeaf2c455a42e465ca17c446afb2661c2
Rafael Campos Nunes <rafaelnunes@engineer.com>
2020-08-13 14:41:08 -0300

    Add the response models from the SuperAPI
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/superapi/model.go
+++ src/superapi/model.go
@@ -1,7 +1,34 @@
 package superapi
 
 type (
+	// Response represents a response from SuperAPI with the needed fields.
 	Response struct {
-		
+		ID string `json:"id"`
+		Name string `json:"name"`
+
+		Image Image `json:"image"`
+		Powerstats Powerstats `json:"powerstats"`
+		Connections Connections `json:"connections"`
+	}
+
+	// Powerstats represents the stats of a hero
+	Powerstats struct {
+		Intelligence string `json:"intelligence"`
+		Strength string `json:"strength"`
+		Speed string `json:"speed"`
+		Durability string `json:"durability"`
+		Power string `json:"powerstats"`
+		Combat string `json:"combat"`
+	}
+
+	// Connections represents the connections that a hero has
+	Connections struct {
+		Affiliation string `json:"affiliation"`
+		Relatives string `json:"relatives"`
+	}
+
+	// Image is, of course, the image of the hero
+	Image struct {
+		URL string `json:"url"`
 	}
 )
